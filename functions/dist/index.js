d755627d4bd97e97d10b04782ac2ac97326223de
Hata <hato6502@gmail.com>
2019-09-26 23:17:33 +0900

    Apollo Server を導入しました。

--- functions/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function helloHandler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("Hello")
-  };
-}
-exports.helloHandler = helloHandler;
---
+++ functions/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_lambda_1 = require("apollo-server-lambda");
+const typeDefs = apollo_server_lambda_1.gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => "hello"
+  }
+};
+const server = new apollo_server_lambda_1.ApolloServer({
+  typeDefs,
+  resolvers
+});
+exports.graphQLHandler = server.createHandler();
